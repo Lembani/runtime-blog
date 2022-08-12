0404b003d462a94462792e4b3668a7439bf2eb9e
Lembani <Lembanisakala@gmail.com>
2022-08-12 16:52:54 +0200

    Create posts migration

---
+++ db/migrate/20220812142716_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.references :user, null: false, foreign_key: true
+      t.integer :commentsCounter
+      t.integer :likesCounter
+
+
+      t.timestamps
+    end
+  end
+end
