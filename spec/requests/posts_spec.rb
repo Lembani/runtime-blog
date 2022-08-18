f1962169d2381d289637139e0ae957a3cd7d0fd1
Lembani <Lembanisakala@gmail.com>
2022-08-18 22:47:25 +0200

    Get user post details

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,3 +16,17 @@ describe 'GET /users/:id/posts' do
   end
 end
 
+describe 'GET /users/:id/posts/:id' do
+  it 'returns http success status' do
+    get '/users/1/posts/1'
+    expect(response).to have_http_status(:success)
+  end
+  it 'renders the show template' do
+    get '/users/1/posts/1'
+    expect(response).to render_template('show')
+  end
+  it 'renders the text in a post' do
+    get '/users/1/posts/1'
+    expect(response.body).to include('Detailed Post info here')
+  end
+end
