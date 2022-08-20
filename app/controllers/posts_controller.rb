c98349ba4560d65357fe4b701c852e98e45c66ff
Lembani <Lembanisakala@gmail.com>
2022-08-20 23:39:01 +0200

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,31 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+    @params = params
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to user_posts_path(id: @post.id, author_id: @post.author_id,
+                                  comments_counter: @post.comments_counter,
+                                  likes_counter: @post.likes_counter)
+
+    else
+      render :new, status: :unprocessable_entity, content_type: 'text/html'
+      headers['Content-Type'] = 'text/html'
+    end
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
   end
 end
