441b134dc98b7c0bb901ddbb29bb40267380b3cf
Lembani <Lembanisakala@gmail.com>
2022-08-24 17:13:11 +0200

    Update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @recent_posts = Post.where(author_id: params[:id]).order(created_at: :desc).limit(3)
+    @recent_posts = Post.includes(:author, :comments,
+                                  :likes).where(author_id: params[:id]).order(created_at: :desc).limit(3)
   end
 end
