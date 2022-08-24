b7f37421796ab5543ae7ead023e71e31cd15eb50
Lembani <Lembanisakala@gmail.com>
2022-08-24 15:34:58 +0200

    Update Likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,8 @@
 class LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
-    redirect_to user_posts_path(id: @like.post_id, user_id: current_user) if @like.save
+    @author = current_user.id
+    redirect_to user_posts_path(id: @like.post_id, user_id: @author) if @like.save
   end
 
   private
